3071fb650bf57fc1c1c5029e074278c2a2a74f95
Alberto Tarantino <albertotarantino75@yahoo.it>
2019-11-20 15:35:28 +0100

    seeds generated

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+
+10.times do
+  Pet.create(name: Faker::Name.name, address: Faker::Address.full_address,
+    breed: Faker::Creature::Animal.name, age: rand(1..20), description: Faker::Faker::Quote.most_interesting_man_in_the_world, user_id:1 )
+end
+
+# above .sample one random user from User.all
+
