08dd06b49746c6456cd9d4b546851feec159e172
Elena-Learning <elena.urban.professionell@gmail.com>
2019-11-20 12:42:50 +0100

    updated show view and referring files

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,6 +1,8 @@
 class Pet < ApplicationRecord
   belongs_to :user
   has_many :rents, dependent: :destroy
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 
   validates :name, presence: true
   validates :description, presence: true
