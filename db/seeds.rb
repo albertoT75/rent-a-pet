08502a1a6db98522d4609c2751b4f6fef5ac5356
albertoT75 <38999136+albertoT75@users.noreply.github.com>
2019-11-20 16:17:10 +0100

    Seeds generate (#16)
    * seeds generated
    
    * implemented working seed
    
    * seeds ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,24 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
-#
+puts "Destroy previous users"
+User.destroy_all
+puts "Destroy previous users"
+Pet.destroy_all
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+USER = User.create(username: "Leon", email: "leon@example.com", password: "123456")
+
+10.times do
+  Pet.create(
+    name: Faker::Name.name,
+    address: Faker::Address.full_address,
+    breed: Faker::Creature::Animal.name,
+    age: rand(1..20),
+    description: Faker::Quote.most_interesting_man_in_the_world,
+    user_id: USER.id )
+ # puts "created pet number #{counter}"
+end
+
+# above .sample one random user from User.all
+
