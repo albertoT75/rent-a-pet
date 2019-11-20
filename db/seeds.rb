55f86d269fa5f7d0e1a22b98c6b941e94009e3c6
Alberto Tarantino <albertotarantino75@yahoo.it>
2019-11-20 16:13:55 +0100

    seeds ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,17 +9,16 @@ Pet.destroy_all
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 USER = User.create(username: "Leon", email: "leon@example.com", password: "123456")
-counter = 0
+
 10.times do
-  counter += 1
- Pet.create(
+  Pet.create(
     name: Faker::Name.name,
     address: Faker::Address.full_address,
     breed: Faker::Creature::Animal.name,
     age: rand(1..20),
     description: Faker::Quote.most_interesting_man_in_the_world,
     user_id: USER.id )
- puts "created pet number #{counter}"
+ # puts "created pet number #{counter}"
 end
 
 # above .sample one random user from User.all
