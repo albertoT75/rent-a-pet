006f50a1286f510474d6794c9ec0d81fbcd2dc79
Alberto Tarantino <albertotarantino75@yahoo.it>
2019-11-20 11:17:49 +0100

    implemented working index page for pet

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -2,7 +2,7 @@ class PetsController < ApplicationController
   before_action :set_pet, only: [:show, :edit, :update]
 
   def index
-    @pets = policy.scope(Pet)
+    @pets = policy_scope(Pet)
   end
 
   def new
