89ed213b2996dca9a20038c9e220f022b203c9d0
Andrei Tigai <andrey.tigay@gmail.com>
2019-11-21 15:08:51 +0100

    add price to pets#show view (#22)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_20_105923) do
+ActiveRecord::Schema.define(version: 2019_11_21_122716) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2019_11_20_105923) do
     t.string "address"
     t.float "latitude"
     t.float "longitude"
+    t.decimal "price"
     t.index ["user_id"], name: "index_pets_on_user_id"
   end
 
@@ -35,7 +36,6 @@ ActiveRecord::Schema.define(version: 2019_11_20_105923) do
     t.bigint "pet_id"
     t.date "start_period"
     t.date "end_period"
-    t.decimal "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["pet_id"], name: "index_rents_on_pet_id"
